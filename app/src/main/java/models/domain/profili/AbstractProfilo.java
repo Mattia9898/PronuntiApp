c14a54c884bb5760d502b5b00be2b752be5b37c1
Mattia9898 <mattiapetragallo00@gmail.com>
2025-08-04 19:09:17 +0200

    updated class AbstractProfilo: deleted unused method setIdProfilo

--- app/src/main/java/models/domain/profili/AbstractProfilo.java
+++ app/src/main/java/models/domain/profili/AbstractProfilo.java
@@ -1,9 +1,11 @@
 package models.domain.profili;
 
-import java.util.HashMap;
-import java.util.Map;
 
 import models.database.costantiDB.CostantiDBAbstractProfilo;
+
+import java.util.Map;
+import java.util.HashMap;
+
 
 public abstract class AbstractProfilo implements Profilo {
 
@@ -19,7 +21,6 @@ public abstract class AbstractProfilo implements Profilo {
 
     protected String password;
 
-    public AbstractProfilo() {}
 
     public AbstractProfilo(String idProfilo, String nome, String cognome, String username, String email, String password) {
         this.idProfilo = idProfilo;
@@ -38,65 +39,68 @@ public abstract class AbstractProfilo implements Profilo {
         this.password = password;
     }
 
+    public AbstractProfilo() {}
+
+
     public String getIdProfilo() {
         return idProfilo;
     }
 
+
     public String getNome() {
         return nome;
-    }
-
-    public String getCognome() {
-        return cognome;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setIdProfilo(String idProfilo) {
-        this.idProfilo = idProfilo;
     }
 
     public void setNome(String nome) {
         this.nome = nome;
     }
 
+
+    public String getCognome() {
+        return cognome;
+    }
+
     public void setCognome(String cognome) {
         this.cognome = cognome;
+    }
+
+
+    public String getUsername() {
+        return username;
     }
 
     public void setUsername(String username) {
         this.username = username;
     }
 
+
+    public String getEmail() {
+        return email;
+    }
+
     public void setEmail(String email) {
         this.email = email;
+    }
+
+
+    public String getPassword() {
+        return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
     }
 
+
     @Override
     public Map<String, Object> toMap() {
-        Map<String, Object> entityMap = new HashMap<>();
-
-        //entityMap.put(CostantiDBProfiloAbstract.ID_PROFILO, this.idProfilo);
-        entityMap.put(CostantiDBAbstractProfilo.NOME, this.nome);
-        entityMap.put(CostantiDBAbstractProfilo.COGNOME, this.cognome);
-        entityMap.put(CostantiDBAbstractProfilo.USERNAME, this.username);
-        entityMap.put(CostantiDBAbstractProfilo.EMAIL, this.email);
-        entityMap.put(CostantiDBAbstractProfilo.PASSWORD, this.password);
-        return entityMap;
+        Map<String, Object> map = new HashMap<>();
+        map.put(CostantiDBAbstractProfilo.NOME, this.nome);
+        map.put(CostantiDBAbstractProfilo.COGNOME, this.cognome);
+        map.put(CostantiDBAbstractProfilo.USERNAME, this.username);
+        map.put(CostantiDBAbstractProfilo.EMAIL, this.email);
+        map.put(CostantiDBAbstractProfilo.PASSWORD, this.password);
+        return map;
     }
 
 }
