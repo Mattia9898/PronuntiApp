caf6f89aa4faf510240f9c2f0e8da5e02d1470a5
Mattia9898 <mattiapetragallo00@gmail.com>
2025-08-14 20:07:07 +0200

    updated class: AppuntamentiGenitoreAdapter; updated file layout: item_appuntamento_in_genitore.xml

--- app/src/main/java/views/fragment/userGenitore/appuntamenti/AppuntamentiGenitoreAdapter.java
+++ app/src/main/java/views/fragment/userGenitore/appuntamenti/AppuntamentiGenitoreAdapter.java
@@ -1,94 +1,89 @@
 package views.fragment.userGenitore.appuntamenti;
 
-import android.view.LayoutInflater;
-
-import android.view.View;
-
-import android.view.ViewGroup;
-
-import android.widget.TextView;
-
-import androidx.cardview.widget.CardView;
-
-import androidx.recyclerview.widget.RecyclerView;
-
-import java.time.LocalDate;
-
-import java.time.LocalDateTime;
-
-import java.time.LocalTime;
-
-import java.time.format.DateTimeFormatter;
-
-import java.util.List;
 
 import it.uniba.dib.pronuntiapp.R;
 
+import java.util.List;
+import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalTime;
+
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.cardview.widget.CardView;
+
+import android.view.View;
+import android.widget.TextView;
+import android.view.ViewGroup;
+import android.view.LayoutInflater;
+
+
 import models.domain.profili.Appuntamento;
+
 
 public class AppuntamentiGenitoreAdapter extends RecyclerView.Adapter<AppuntamentiGenitoreAdapter.ViewHolder> {
 
-    private List<Appuntamento> appuntamenti;
+    private List<Appuntamento> listAppuntamento;
+
 
     public AppuntamentiGenitoreAdapter(List<Appuntamento> appuntamenti) {
-        this.appuntamenti = appuntamenti;
+        this.listAppuntamento = appuntamenti;
     }
 
-
-    @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_appuntamento_in_genitore, parent, false);
-
-        return new ViewHolder(view);
-    }
-
-    @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-
-        Appuntamento appuntamento = appuntamenti.get(position);
-
-        LocalDateTime now = LocalDateTime.of(LocalDate.now(), LocalTime.now());
-        LocalDateTime appuntamentoDateTime = LocalDateTime.of(appuntamento.getData(), appuntamento.getOra());
-
-        //disabilita se la data è già passata
-        if (appuntamentoDateTime.isBefore(now) || appuntamentoDateTime.isEqual(now)){
-            holder.cardViewAppuntamentoGenitore.setCardBackgroundColor(holder.itemView.getContext().getColor(R.color.hintTextColorDisabled));
-        } else {
-            holder.cardViewAppuntamentoGenitore.setCardBackgroundColor(holder.itemView.getContext().getColor(R.color.colorPrimary));
-        }
-
-        holder.textViewGiornoAppuntamentoGenitore.setText(DateTimeFormatter.ofPattern("dd").format(appuntamento.getData()));
-        holder.textViewMeseAppuntamentoGenitore.setText(DateTimeFormatter.ofPattern("MMMM yyyy").format(appuntamento.getData()));
-        holder.textViewLuogoAppuntamentoGenitore.setText(appuntamento.getLuogo());
-        holder.textViewOraAppuntamentoGenitore.setText(DateTimeFormatter.ofPattern("HH:mm").format(appuntamento.getOra()));
-    }
-
-    @Override
-    public int getItemCount() {
-        return appuntamenti.size();
-    }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
-        private TextView textViewGiornoAppuntamentoGenitore;
+        private TextView dayAppuntamento;
 
-        private TextView textViewMeseAppuntamentoGenitore;
+        private TextView monthAppuntamento;
 
-        private TextView textViewLuogoAppuntamentoGenitore;
+        private TextView placeAppuntamento;
 
-        private TextView textViewOraAppuntamentoGenitore;
+        private TextView timeAppuntamento;
 
         private CardView cardViewAppuntamentoGenitore;
 
         public ViewHolder(View itemView) {
             super(itemView);
             cardViewAppuntamentoGenitore = itemView.findViewById(R.id.cardViewAppuntamentoGenitore);
-            textViewGiornoAppuntamentoGenitore = itemView.findViewById(R.id.textViewGiornoAppuntamentoGenitore);
-            textViewMeseAppuntamentoGenitore = itemView.findViewById(R.id.textViewMeseAppuntamentoGenitore);
-            textViewLuogoAppuntamentoGenitore = itemView.findViewById(R.id.textViewLuogoAppuntamentoGenitore);
-            textViewOraAppuntamentoGenitore = itemView.findViewById(R.id.textViewOraAppuntamentoGenitore);
+            dayAppuntamento = itemView.findViewById(R.id.dayAppuntamento);
+            monthAppuntamento = itemView.findViewById(R.id.monthAppuntamento);
+            placeAppuntamento = itemView.findViewById(R.id.placeAppuntamento);
+            timeAppuntamento = itemView.findViewById(R.id.timeAppuntamento);
         }
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_appuntamento_in_genitore, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+        Appuntamento appuntamento = listAppuntamento.get(position);
+
+        LocalDateTime actualDate = LocalDateTime.of(LocalDate.now(), LocalTime.now());
+        LocalDateTime appuntamentoDate = LocalDateTime.of(appuntamento.getData(), appuntamento.getOra());
+
+        //disabilita se la data è già passata
+        if (appuntamentoDate.isBefore(actualDate) || appuntamentoDate.isEqual(actualDate)){
+            holder.cardViewAppuntamentoGenitore.setCardBackgroundColor(holder.itemView.getContext().getColor(R.color.hintTextColorDisabled));
+        } else {
+            holder.cardViewAppuntamentoGenitore.setCardBackgroundColor(holder.itemView.getContext().getColor(R.color.colorPrimary));
+        }
+
+        holder.dayAppuntamento.setText(DateTimeFormatter.ofPattern("dd").format(appuntamento.getData()));
+        holder.monthAppuntamento.setText(DateTimeFormatter.ofPattern("MMMM yyyy").format(appuntamento.getData()));
+        holder.placeAppuntamento.setText(appuntamento.getLuogo());
+        holder.timeAppuntamento.setText(DateTimeFormatter.ofPattern("HH:mm").format(appuntamento.getOra()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return listAppuntamento.size();
     }
 
 }
