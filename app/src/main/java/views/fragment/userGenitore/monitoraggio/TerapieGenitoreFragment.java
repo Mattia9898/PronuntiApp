58625823712ee74d93faed54599d4f7aa3e456fa
Mattia9898 <mattiapetragallo00@gmail.com>
2025-08-18 10:58:20 +0200

    updated class: TerapieGenitoreFragment; updated layout file: fragment_terapie.xml

--- app/src/main/java/views/fragment/userGenitore/monitoraggio/TerapieGenitoreFragment.java
+++ app/src/main/java/views/fragment/userGenitore/monitoraggio/TerapieGenitoreFragment.java
@@ -1,69 +1,60 @@
 package views.fragment.userGenitore.monitoraggio;
 
-import android.os.Bundle;
-
-import android.util.Log;
-
-import android.view.LayoutInflater;
-
-import android.view.View;
-
-import android.view.ViewGroup;
-
-import android.widget.FrameLayout;
-
-import android.widget.LinearLayout;
-
-import androidx.annotation.NonNull;
-
-import androidx.annotation.Nullable;
-
-import androidx.fragment.app.FragmentContainerView;
-
-import androidx.lifecycle.ViewModelProvider;
-
-import java.time.LocalDate;
-
-import java.util.ArrayList;
-
-import java.util.Comparator;
-
-import java.util.List;
-
-import java.util.stream.Collectors;
 
 import it.uniba.dib.pronuntiapp.R;
 
-import models.domain.terapie.Terapia;
+import android.os.Bundle;
 
-import viewsModels.genitoreViewsModels.GenitoreViewsModels;
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
 
 import views.fragment.AbstractNavigazioneFragment;
+import viewsModels.genitoreViewsModels.GenitoreViewsModels;
+import models.domain.terapie.Terapia;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.Comparator;
+
+import androidx.lifecycle.ViewModelProvider;
+import androidx.fragment.app.FragmentContainerView;
+
+import android.view.LayoutInflater;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.view.ViewGroup;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import java.time.LocalDate;
+
 
 public class TerapieGenitoreFragment extends AbstractNavigazioneFragment {
 
-    private FragmentContainerView fragmentContainerViewMonitoraggio;
+    private FragmentContainerView navigationTherapies;
 
-    private GenitoreViewsModels mGenitoreViewModel;
+    private FragmentContainerView monitoring;
 
-    private FragmentContainerView fragmentContainerViewNavTerapie;
+    private GenitoreViewsModels genitoreViewsModels;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         super.onCreateView(inflater, container, savedInstanceState);
-
         View view = inflater.inflate(R.layout.fragment_terapie, container, false);
 
-        fragmentContainerViewNavTerapie = view.findViewById(R.id.fragmentContainerViewNavTerapie);
+        navigationTherapies = view.findViewById(R.id.navigationTherapies);
+        monitoring = view.findViewById(R.id.monitoring);
 
+        genitoreViewsModels = new ViewModelProvider(requireActivity()).get(GenitoreViewsModels.class);
 
-        fragmentContainerViewMonitoraggio = view.findViewById(R.id.fragmentContainerViewMonitoraggio);
-        mGenitoreViewModel = new ViewModelProvider(requireActivity()).get(GenitoreViewsModels.class);
+        FragmentContainerView.MarginLayoutParams marginLayoutParams =
+                (FragmentContainerView.MarginLayoutParams) monitoring.getLayoutParams();
 
-        FragmentContainerView.MarginLayoutParams params = (FragmentContainerView.MarginLayoutParams) fragmentContainerViewMonitoraggio.getLayoutParams();
-        params.setMargins(0, 0, 0, getContext().getResources().getDimensionPixelSize(R.dimen.nav_bar_height));
+        marginLayoutParams.setMargins(0, 0, 0, getContext().getResources().getDimensionPixelSize(R.dimen.nav_bar_height));
 
         return view;
 
@@ -73,28 +64,27 @@ public class TerapieGenitoreFragment extends AbstractNavigazioneFragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 
         super.onViewCreated(view, savedInstanceState);
+        getChildFragmentManager().beginTransaction().
+                replace(R.id.navigationTherapies, new NavigazioneTerapieGenitoreFragment()).commit();
 
-        getChildFragmentManager().beginTransaction().replace(R.id.fragmentContainerViewNavTerapie, new NavigazioneTerapieGenitoreFragment()).commit();
+        genitoreViewsModels.getPazienteLiveData().observe(getViewLifecycleOwner(), paziente -> {
 
-
-
-        mGenitoreViewModel.getPazienteLiveData().observe(getViewLifecycleOwner(), paziente -> {
-
-            if(mGenitoreViewModel.getPazienteLiveData().getValue().getTerapie() != null) {
-                mGenitoreViewModel.getPazienteLiveData().getValue().getTerapie().sort(Comparator.comparing(Terapia::getDataInizioTerapia));
+            if(genitoreViewsModels.getPazienteLiveData().getValue().getTerapie() != null) {
+                genitoreViewsModels.getPazienteLiveData().getValue().
+                        getTerapie().sort(Comparator.comparing(Terapia::getDataInizioTerapia));
             }
 
-            int risultatoIndice = mGenitoreViewModel.getIndiceUltimaTerapia();
+            int result = genitoreViewsModels.getIndiceUltimaTerapia();
 
-            if(risultatoIndice != -1) {
+            if(result != -1) {
+
                 Bundle bundle = new Bundle();
-
-                bundle.putInt("indiceTerapiaScelta",risultatoIndice);
+                bundle.putInt("terapiaScelta", result);
 
                 MonitoraggioGenitoreFragment monitoraggioGenitoreFragment = new MonitoraggioGenitoreFragment();
                 monitoraggioGenitoreFragment.setArguments(bundle);
 
-                getChildFragmentManager().beginTransaction().replace(R.id.fragmentContainerViewMonitoraggio, monitoraggioGenitoreFragment).commit();
+                getChildFragmentManager().beginTransaction().replace(R.id.monitoring, monitoraggioGenitoreFragment).commit();
 
             }
 
