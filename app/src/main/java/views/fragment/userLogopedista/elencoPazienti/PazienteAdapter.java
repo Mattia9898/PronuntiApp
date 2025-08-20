e179194fd7edda73af26940ffcea562aa7fab6f1
Mattia9898 <mattiapetragallo00@gmail.com>
2025-08-20 12:28:38 +0200

    updated class: PazienteAdapter; updated layout file: item_paziente_in_lista_logopedista.xml

--- app/src/main/java/views/fragment/userLogopedista/elencoPazienti/PazienteAdapter.java
+++ app/src/main/java/views/fragment/userLogopedista/elencoPazienti/PazienteAdapter.java
@@ -1,60 +1,57 @@
 package views.fragment.userLogopedista.elencoPazienti;
 
-import android.os.Bundle;
-
-import android.view.LayoutInflater;
-
-import android.view.View;
-
-import android.view.ViewGroup;
-
-import android.widget.Filter;
-
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-
-import androidx.cardview.widget.CardView;
-
-import androidx.recyclerview.widget.RecyclerView;
-
-import java.util.ArrayList;
-
-import java.util.List;
 
 import it.uniba.dib.pronuntiapp.R;
 
-import models.domain.profili.Paziente;
+import androidx.annotation.NonNull;
+
+import android.os.Bundle;
+
+import java.util.List;
+import java.util.ArrayList;
 
 import views.fragment.adapter.Navigation;
 
+import models.domain.profili.Paziente;
+
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.cardview.widget.CardView;
+
+import android.view.View;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Filter;
+
+
 public class PazienteAdapter extends RecyclerView.Adapter<PazienteAdapter.PazienteViewHolder> {
 
-    private List<Paziente> pazientiCopia;
+    private List<Paziente> patient;
 
-    private List<Paziente> pazientiFull;
+    private List<Paziente> fullListPatients;
 
-    private CardView lastClickedCardView = null;
+    private CardView lastCardView = null;
 
     private Navigation navigation;
 
-    public PazienteAdapter(List<Paziente> pazienti, Navigation navigation) {
+
+    public PazienteAdapter(List<Paziente> listPatient, Navigation navigation) {
         this.navigation = navigation;
-        if (pazienti == null) {
-            this.pazientiCopia = new ArrayList<>();
-            this.pazientiFull = new ArrayList<>();
+        if (listPatient == null) {
+            this.patient = new ArrayList<>();
+            this.fullListPatients = new ArrayList<>();
         } else {
-            this.pazientiCopia = new ArrayList<>(pazienti);
-            pazientiCopia.sort((p1,p2)-> {
-                int confrontoCognome = p1.getCognome().compareTo(p2.getCognome());
-                if(confrontoCognome == 0){
+            this.patient = new ArrayList<>(listPatient);
+            patient.sort((p1,p2)-> {
+                int compareSurname = p1.getCognome().compareTo(p2.getCognome());
+                if (compareSurname == 0){
                     return p1.getNome().compareTo(p2.getCognome());
                 }else {
-                    return confrontoCognome;
+                    return compareSurname;
                 }
             });
 
-            this.pazientiFull = new ArrayList<>(pazientiCopia);
+            this.fullListPatients = new ArrayList<>(patient);
         }
     }
 
@@ -68,103 +65,107 @@ public class PazienteAdapter extends RecyclerView.Adapter<PazienteAdapter.Pazien
     @Override
     public void onBindViewHolder(@NonNull PazienteViewHolder holder, int position) {
 
-        Paziente paziente = pazientiCopia.get(position);
+        Paziente paziente = patient.get(position);
 
-        holder.textViewNomePaziente.setText(paziente.getNome());
-        holder.textViewCognomePaziente.setText(paziente.getCognome());
-        holder.textViewDataNascitaPaziente.setText(paziente.getDataNascita().toString());
-        holder.textViewSessoPaziente.setText(Character.toString(paziente.getSesso()));
+        holder.namePatient.setText(paziente.getNome());
+        holder.surnamePatient.setText(paziente.getCognome());
+        holder.birthdatePatient.setText(paziente.getDataNascita().toString());
+        holder.sexPatient.setText(Character.toString(paziente.getSesso()));
 
-        holder.textViewNomePaziente.setOnClickListener(v -> selezionePaziente(holder));
-        holder.textViewCognomePaziente.setOnClickListener(v -> selezionePaziente(holder));
-        holder.textViewDataNascitaPaziente.setOnClickListener(v -> selezionePaziente(holder));
-        holder.textViewSessoPaziente.setOnClickListener(v -> selezionePaziente(holder));
-        holder.cardViewPazienteInListaLogopedista.setOnClickListener(v -> selezionePaziente(holder));
-    }
+        holder.namePatient.setOnClickListener(v -> selectionPatient(holder));
+        holder.surnamePatient.setOnClickListener(v -> selectionPatient(holder));
+        holder.birthdatePatient.setOnClickListener(v -> selectionPatient(holder));
+        holder.sexPatient.setOnClickListener(v -> selectionPatient(holder));
 
-    private void selezionePaziente(PazienteViewHolder holder) {
-
-        //metodo di onclick sul paziente nella lista dei pazienti
-        holder.cardViewPazienteInListaLogopedista.setCardBackgroundColor(holder.itemView.getContext().getColor(R.color.primaryColorSoft));
-
-        if(lastClickedCardView != null && lastClickedCardView != holder.cardViewPazienteInListaLogopedista){
-            lastClickedCardView.setCardBackgroundColor(holder.itemView.getContext().getColor(R.color.colorPrimary));
-        }
-        lastClickedCardView = holder.cardViewPazienteInListaLogopedista;
-
-        String idPazienteSelezionato = pazientiCopia.get(holder.getAdapterPosition()).getIdProfilo();
-        String nomePazienteSelezionato = pazientiCopia.get(holder.getAdapterPosition()).getNome();
-        String cognomePazienteSelezionato = pazientiCopia.get(holder.getAdapterPosition()).getCognome();
-
-        Bundle bundle = new Bundle();
-        bundle.putString("idPaziente",idPazienteSelezionato);
-        bundle.putString("nomePaziente",nomePazienteSelezionato);
-        bundle.putString("cognomePaziente",cognomePazienteSelezionato);
-        navigation.navigationId(R.id.action_pazientiFragment_to_schedaPazienteFragment, bundle);
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return pazientiCopia.size();
-    }
-
-    public Paziente getItem(int position) {
-        return pazientiCopia.get(position);
-    }
-
-    public static class PazienteViewHolder extends RecyclerView.ViewHolder {
-
-        TextView textViewNomePaziente;
-
-        TextView textViewCognomePaziente;
-
-        TextView textViewDataNascitaPaziente;
-
-        TextView textViewSessoPaziente;
-
-        CardView cardViewPazienteInListaLogopedista;
-
-        public PazienteViewHolder(@NonNull View itemView) {
-            super(itemView);
-            cardViewPazienteInListaLogopedista = itemView.findViewById(R.id.cardViewPazienteInListaLogopedista);
-            textViewNomePaziente = itemView.findViewById(R.id.textViewNomePaziente);
-            textViewCognomePaziente = itemView.findViewById(R.id.textViewCognomePaziente);
-            textViewDataNascitaPaziente = itemView.findViewById(R.id.textViewDataNascitaPaziente);
-            textViewSessoPaziente = itemView.findViewById(R.id.textViewSessoPaziente);
-        }
+        holder.cardViewPatient.setOnClickListener(v -> selectionPatient(holder));
     }
 
     public Filter getFilter() {
         return new PazienteFilter();
     }
 
+    public Paziente getItem(int position) {
+        return patient.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return patient.size();
+    }
+
+
+    private void selectionPatient(PazienteViewHolder holder) {
+
+        // onclick method on patient in list patients
+        holder.cardViewPatient.setCardBackgroundColor(holder.itemView.getContext().getColor(R.color.primaryColorSoft));
+
+        if(lastCardView != null && lastCardView != holder.cardViewPatient){
+            lastCardView.setCardBackgroundColor(holder.itemView.getContext().getColor(R.color.colorPrimary));
+        }
+        lastCardView = holder.cardViewPatient;
+
+        String idSelectedPatient = patient.get(holder.getAdapterPosition()).getIdProfilo();
+        String nameSelectedPatient = patient.get(holder.getAdapterPosition()).getNome();
+        String surnameSelectedPatient = patient.get(holder.getAdapterPosition()).getCognome();
+
+        Bundle bundle = new Bundle();
+
+        bundle.putString("idPaziente", idSelectedPatient);
+        bundle.putString("namePatient", nameSelectedPatient);
+        bundle.putString("surnamePatient", surnameSelectedPatient);
+        navigation.navigationId(R.id.action_pazientiFragment_to_schedaPazienteFragment, bundle);
+
+    }
+
+    public static class PazienteViewHolder extends RecyclerView.ViewHolder {
+
+        TextView namePatient;
+
+        TextView surnamePatient;
+
+        TextView birthdatePatient;
+
+        TextView sexPatient;
+
+        CardView cardViewPatient;
+
+        public PazienteViewHolder(@NonNull View itemView) {
+            super(itemView);
+            cardViewPatient = itemView.findViewById(R.id.cardViewPatient);
+            namePatient = itemView.findViewById(R.id.namePatient);
+            surnamePatient = itemView.findViewById(R.id.surnamePatient);
+            birthdatePatient = itemView.findViewById(R.id.birthdatePatient);
+            sexPatient = itemView.findViewById(R.id.sexPatient);
+        }
+    }
+
     private class PazienteFilter extends Filter {
 
         @Override
         protected FilterResults performFiltering(CharSequence charSequence) {
-            List<Paziente> filteredPazienti = new ArrayList<>();
+            List<Paziente> filteredPatients = new ArrayList<>();
             if (charSequence == null || charSequence.length() == 0) {
-                filteredPazienti.addAll(pazientiFull);
+                filteredPatients.addAll(fullListPatients);
             } else {
                 String query = charSequence.toString().toLowerCase();
-                for (Paziente paziente : pazientiFull) {
-                    String nome = paziente.getNome().toLowerCase();
-                    String cognome = paziente.getCognome().toLowerCase();
-                    if (nome.contains(query) || cognome.contains(query)) {
-                        filteredPazienti.add(paziente);
+                for (Paziente paziente : fullListPatients) {
+                    String name = paziente.getNome().toLowerCase();
+                    String surname = paziente.getCognome().toLowerCase();
+                    if (name.contains(query) || surname.contains(query)) {
+                        filteredPatients.add(paziente);
                     }
                 }
             }
+
             FilterResults filterResults = new FilterResults();
-            filterResults.values = filteredPazienti;
+            filterResults.values = filteredPatients;
             return filterResults;
         }
 
         @Override
         protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
-            pazientiCopia.clear();
-            pazientiCopia.addAll((List<Paziente>) filterResults.values);
+            patient.clear();
+            patient.addAll((List<Paziente>) filterResults.values);
             notifyDataSetChanged();
         }
     }
